5bf0dc885db0032e21860becc3c797c36abaf468
riyan11998j <zebang57@gmail.com>
2024-11-14 21:08:25 +0700

    noval

---
+++ laporan.php
@@ -0,0 +1,40 @@
+<h1 class="mt-4">Laporan Peminjaman Buku</h1>
+<div class="card">
+    <div class="card-body">
+        <div class="row">
+            <div class="col-md-12">
+                <a href="cetak.php" target="_blank" class="btn btn-primary mb-3"><i class="fa fa-print"></i>Cetak Data</a>
+                <table class="table table-bordered table-striped" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>User</th>
+                            <th>Buku</th>
+                            <th>Tanggal Peminjaman</th>
+                            <th>Tanggal Pengembalian</th>
+                            <th>Status Peminjaman</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $i = 1;
+                        $query = mysqli_query($koneksi, "SELECT*FROM peminjaman LEFT JOIN user on user.id_user = peminjaman.id_user LEFT JOIN buku on buku.id_buku = peminjaman.id_buku");
+                        while ($data = mysqli_fetch_array($query)) {
+                        ?>
+                            <tr>
+                                <td><?php echo $i++; ?></td>
+                                <td><?php echo ($data['nama']); ?></td>
+                                <td><?php echo ($data['judul']); ?></td>
+                                <td><?php echo ($data['tanggal_peminjaman']); ?></td>
+                                <td><?php echo ($data['tanggal_pengembalian']); ?></td>
+                                <td><?php echo ($data['status_peminjaman']); ?></td>
+                            </tr>
+                        <?php
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
